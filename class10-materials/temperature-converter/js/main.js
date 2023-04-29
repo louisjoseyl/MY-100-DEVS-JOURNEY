2b5e00ef81bcf19866c0785d40cdea878eb76805
Louis Joseph <louisjosephleadway@gmail.com>
2023-04-29 16:31:50 +0100

    temperature converter completed

--- class10-materials/temperature-converter/js/main.js
+++ class10-materials/temperature-converter/js/main.js
@@ -1,1 +1,26 @@
 //Write your pseduo code first! 
+
+//click event (smurf)
+document.querySelector('#zebra').addEventListener('click', convert)
+
+// declare your function
+function convert(){
+  //get the values from the input
+  const celsius = document.querySelector('#brainStorm').value
+  //quick maths convert it 
+  const convertedValue = celsius * 9/5 + 32
+  // display the value
+  document.querySelector('#talkThatTalk').innerHTML = convertedValue
+}
+
+
+
+/* function convert(){
+    //get the values from the input
+  //quick maths convert it 
+  const convertedValue = document.querySelector('#brainStorm').value * 9/5 + 32
+  // display the value
+  console.log(convertedValue)
+
+}*/
+ 
